4a316a599e7e3bf938896f6dd5cfe37849ccaa1a
Mesut Yılmaz <mesutasimoglu@hotmail.com>
2018-11-05 01:17:39 +0300

    MVC ye uyumlu hale getirmeye calıstık :P

--- ToDoList/Controllers/TodoListViewController.swift
+++ ToDoList/Controllers/TodoListViewController.swift
@@ -13,14 +13,29 @@ class TodoListViewController: UITableViewController
     
     let defaults = UserDefaults.standard
     
-    var itemArray = ["Takım elbise al", "Her sabah dişlerini fırçala","Sürekli birşeyler üretmek için uğraş"]
+    var itemArray = [Item()]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        let newItem1 = Item()
+        newItem1.tittle = "Mesut"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.tittle = "Yılmaz"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.tittle = "Cok iyi"
+        itemArray.append(newItem3)
+        itemArray.remove(at: 0)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
+
+    
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,24 +43,39 @@ class TodoListViewController: UITableViewController
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item  = itemArray[indexPath.row]
+        cell.textLabel?.text = item.tittle
+        
+        
+        
+        /*if item.done == true {
+            cell.accessoryType = .checkmark
+        }else {
+            cell.accessoryType = .none
+        }*/
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
+        
         return cell
     }
     
+    
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
         
-        if   tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-
-        }
-
+    
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        // Yukardaki kodun uzun yolu
+        /*if itemArray[indexPath.row].done == false {
+            itemArray[indexPath.row].done = true
+        }else{
+            itemArray[indexPath.row].done = false
+        }*/
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -59,7 +89,9 @@ class TodoListViewController: UITableViewController
         let action = UIAlertAction(title: "Ekle", style: .default) { (action) in
             
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.tittle = textField.text!
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ ToDoList/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToDoList
+//
+//  Created by Mesut Yılmaz on 4.11.2018.
+//  Copyright © 2018 Mesut Yılmaz. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var tittle : String = ""
+    var done : Bool = false
+    
+}
